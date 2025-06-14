fccc311fe9aa5d3f7cbc0a8c576750c90f805d59
Zeeshan <zeeshan.mukram6@gmail.com>
2025-06-14 19:25:09 +0530

    Created Product entity

---
+++ src/main/java/com/zeeshan/ecommerce_backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.zeeshan.ecommercebackend.repository;
+
+import com.zeeshan.ecommercebackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/com/zeeshan/model/Product.java
@@ -0,0 +1,23 @@
+package com.zeeshan.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int stock;
+}
